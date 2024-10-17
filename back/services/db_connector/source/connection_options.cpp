9e4be02ff44c3cb3ab10d0c67aa7692efcf58e5d
kotb <zavgmaxden@mail.ru>
2024-10-17 22:59:59 +0300

    divide connection options from accessor on same level

---
+++ back/services/db_connector/source/connection_options.cpp
@@ -0,0 +1,63 @@
+#include "connection_options.h"
+
+namespace srv
+{
+namespace db
+{
+
+constexpr std::string_view DEFAULT_ADDRESS = "127.0.0.1";
+constexpr uint32_t DEFAULT_PORT = 5432;
+constexpr std::string_view DEFAULT_DBNAME = "postgres";
+constexpr std::string_view DEFAULT_USER = "postgres";
+constexpr std::string_view DEFAULT_PASSWORD = "defaultpassword";  // doesn't make much sense
+
+ConnectionOptions::ConnectionOptions(std::shared_ptr<srv::ITracer> tracer)
+    : srv::tracer::TracerProvider(std::move(tracer))
+    , m_address(DEFAULT_ADDRESS)
+    , m_port(DEFAULT_PORT)
+    , m_dbname(DEFAULT_DBNAME)
+    , m_user(DEFAULT_USER)
+    , m_password(DEFAULT_PASSWORD)
+{
+}
+
+void ConnectionOptions::SetSettings(const AccessorSettings& settings)
+{
+    std::unique_lock lock(m_optionsMutex);
+
+    ufa::TryExtractFromOptional(settings.dbmsAddress, m_address);
+    ufa::TryExtractFromOptional(settings.dbmsPort, m_port);
+    ufa::TryExtractFromOptional(settings.dbmsDbname, m_dbname);
+    ufa::TryExtractFromOptional(settings.dbmsUser, m_user);
+    ufa::TryExtractFromOptional(settings.dbmsPassword, m_password);
+
+    m_cachedString.clear();
+}
+
+const std::string& ConnectionOptions::GetConnectionString() const
+{
+    std::shared_lock readLock(m_optionsMutex);
+
+    if (m_cachedString.empty())
+    {
+        readLock.unlock();
+        std::unique_lock writeLock(m_optionsMutex);
+
+        std::stringstream connString;
+        connString << ADDRESS_CONN << '=' << m_address << ' ';
+        connString << DBNAME_CONN << '=' << m_dbname << ' ';
+        connString << PORT_CONN << '=' << m_port << ' ';
+        connString << USER_CONN << '=' << m_user << ' ';
+        connString << PASSWORD_CONN << '=' << m_password;
+
+        m_cachedString = connString.str();
+
+        // lock dies here so return in this block
+        return m_cachedString;
+    }
+
+    return m_cachedString;
+}
+
+}  // namespace db
+}  // namespace srv
---
+++ back/services/db_connector/source/connection_options.h
@@ -0,0 +1,46 @@
+#ifndef H_9C71A225_6ACD_431F_9474_3BAFE1DBB213
+#define H_9C71A225_6ACD_431F_9474_3BAFE1DBB213
+
+#include <memory>
+#include <shared_mutex>
+
+#include <db_connector/accessor.h>
+#include <tracer/tracer_provider.h>
+
+namespace srv
+{
+namespace db
+{
+
+class ConnectionOptions : public srv::tracer::TracerProvider
+{
+public:
+    ConnectionOptions(std::shared_ptr<srv::ITracer> tracer);
+
+    void SetSettings(const AccessorSettings& settings);
+
+    const std::string& GetConnectionString() const;
+
+private:
+    // for constructing connections string
+    static constexpr std::string_view ADDRESS_CONN = "host";
+    static constexpr std::string_view PORT_CONN = "port";
+    static constexpr std::string_view DBNAME_CONN = "dbname";
+    static constexpr std::string_view USER_CONN = "user";
+    static constexpr std::string_view PASSWORD_CONN = "password";
+
+private:
+    mutable std::shared_mutex m_optionsMutex;
+    mutable std::string m_cachedString;
+
+    std::string m_address;
+    uint32_t m_port;
+    std::string m_dbname;
+    std::string m_user;
+    std::string m_password;
+};
+
+}  // namespace db
+}  // namespace srv
+
+#endif  // H_9C71A225_6ACD_431F_9474_3BAFE1DBB213
